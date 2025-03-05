bfa456cb968c1ae2239be5f083c624be2cb19cfe
Augustine Aguworumba <augustine.aguworumba@gmail.com>
2025-03-05 04:52:53 -0800

    Class and Attributes declared in movie.h

---
+++ Movie.h
@@ -0,0 +1,55 @@
+#ifndef _MOVIE_H_
+
+#define _MOVIE_H_
+
+#include <string>
+using namespace std;
+
+
+class Movie
+{
+    private:
+        string movie_name;
+        string movie_rating;
+        int movie_watched;
+    public:
+        //Constructor
+        Movie(string movie_name, string movie_rating, int movie_watched;
+        
+        //Copy constructor
+        Movie(const Movie &source);
+        
+        //Destructor
+        ~Movie();
+        
+        //Setters and Getters
+        void set_name(string movie_name){
+            this-> = movie_name;
+            }
+        string get_name() const{
+            return movie_name;
+            }
+
+        void set_rating(string movie_rating){
+            this-> = movie_rating;
+            }
+        string get_rating() const{
+            return movie_rating;
+            }
+
+        void set_watched(int movie_watched){
+            this-> = movie_watched;
+            }
+        int get_watched() const{
+            return movie_watched;
+            }
+            
+        // Simply increment the movie_watched attribute by 1
+        void increment_watched() { ++movie_watched; }
+
+        // simply displays the movie information ex.) Prison Break, PG-13, 2
+        void display() const;
+        
+};
+
+#endif // _MOVIE_H_
